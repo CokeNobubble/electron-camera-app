a6ac339fb12c2d50312c74629180f28a9fa7eb9f
Lee <1660947451@qq.com>
2024-06-13 15:50:22 +0800

    feat:初始化项目

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <div>1234</div>
+</template>
---
+++ src/renderer/src/main.ts
@@ -0,0 +1,8 @@
+// import './assets/main.css'
+import 'virtual:uno.css'
+import 'reset-css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
